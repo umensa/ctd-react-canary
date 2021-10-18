b444a074dbc2130209a9c4297de949433058f4fd
Mohamed Zouari <mensae@posteo.org>
2021-10-18 08:31:31 +0100

    React Components and JSX

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+
+      <hr />
+
+      <ul>
+        
+      </ul>
     </div>
   );
 }
