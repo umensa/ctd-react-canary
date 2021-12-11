b59d9600b74197bd98ae74c42ca934f7bad46f6c
umensa <mensae@posteo.org>
2021-12-11 19:20:01 +0100

    Add "Remove" Button to List Items

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import TodoListItem from './TodoListItem';
+import React from 'react'
+import TodoListItem from './TodoListItem'
 
-const TodoList = ({todoList}) => (
+const TodoList = ({todoList, onRemoveTodo}) => (
   <ul>
     {todoList.map((list) => (
-      <TodoListItem key={list.id} todo={list.title} />
+      <TodoListItem key={list.id} todo={list} onRemoveTodo={onRemoveTodo} />
     ))}
   </ul>
-);
+)
 
-export default TodoList;
+export default TodoList
